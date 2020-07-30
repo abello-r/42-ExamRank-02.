801c7e07b339908a7c093426e6978fd0463a30c8
Alexander Bello rodriguez <abello-r@c2r4s1.42madrid.com>
2020-07-30 11:44:13 +0200

    final

---
+++ gnl/get_next_line.h
@@ -0,0 +1,12 @@
+# ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H	
+
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int get_next_line(char **line);
+
+#endif
---
+++ gnl/main.c
@@ -0,0 +1,18 @@
+#include "get_next_line_h"
+
+int main(void)
+{
+	int		r;
+	char	*line;
+
+	line = NULL;
+	while ((r = get_next_line(&line)) > 0)
+	{
+		printf("%s\n", line);
+		free(line);
+		line = NULL;
+	}
+	printf("%s", line);
+	free(line);
+	line = NULL;
+}
